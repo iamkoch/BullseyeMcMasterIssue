2c15696e3952616efbc8a3bb292ed7361848a166
Antony Koch <antony.koch@investec.co.uk>
2019-10-18 09:28:10 +0100

    initial commit of hanging project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Bullseye.Internal;
+using McMaster.Extensions.CommandLineUtils;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+namespace BullseyeMcMaster
+{
+    class Program
+    {
+        static void Main(string[] args) =>
+            CommandLineApplication.Execute<Program>(args);
+
+        [Option(ShortName = "sw", Description = "say what")]
+        public string SayWhat { get; }
+
+        public string[] RemainingArgs { get; }
+
+        public void OnExecute()
+        {
+            Target("default", () => Run("echo", SayWhat));
+
+            RunTargetsAndExit(RemainingArgs);
+        }
+    }
+}
